0c09d3802e9e0bf244b510149738817d2b9767ff
Matthew Rizzini <matthewrizzini@gmail.com>
2017-12-14 17:59:44 +0000

    changed format

--- index.js
+++ index.js
@@ -2,63 +2,81 @@
 
 $(document).ready(function() { 
     
-        $('#clearCurrent').click(function() {
-    	    $('#results').html(0);
-    	});
-    
-        $('#decimal').click(function() {
-    	    
-    	});
-    	
-    	$('#add').click(function() {
-    	    $('#results').html();
-    	});
-    
-        $('#zero').click(function() {
-    	    document.getElementById('results').innerHTML =  document.getElementById('results').innerHTML + document.getElementById('zero').value;
-            
-        });
-    
-    	$('#one').click(function() {
-    	 document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('one').value;
-    	   // $('#results').html(1);
-    	});
-    
-        $('#two').click(function() {
-    	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('two').value;
+    var numberInput = "";
+
+    	$("#one").click(function(){
+            // console.log("hey");
+            // numberInput = $(this).val();
+            // console.log(numberInput);
     	});
     	
-    	$('#three').click(function() {
-    	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('three').value;
+    	$("#two").click(function(){
+            // console.log("hey 2");
+            // numberInput = $(this).val();
+            // console.log(numberInput);
     	});
     	
-    	$('#four').click(function() {
-    	  document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('four').value;
-    	});
-    
-        $('#five').click(function() {
-    	  document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('five').value;
-    	});
+
     	
-    	$('#six').click(function() {
-    	 document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('six').value;
-    	});
-    	
-    	$('#seven').click(function() {
-    	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('seven').value;
-    	});
-    
-        $('#eight').click(function() {
-    	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('eight').value;
-    	});
-    	
-    	$('#nine').click(function() {
-    	   document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('nine').value;
-    	});
-    	
-    	
-    	
-    	
+    // 	console.log(numbers);
     	
 });
 //above line ends the document ready
+
+
+
+    
+    //     $('#clearCurrent').click(function() {
+    // 	    $('#results').html(0);
+    // 	});
+    
+    //     $('#decimal').click(function() {
+    	    
+    // 	});
+    	
+    // 	$('#add').click(function() {
+    // 	    $('#results').html();
+    // 	});
+    
+    //     $('#zero').click(function() {
+    // 	     document.getElementById('results').innerHTML =  document.getElementById('results').innerHTML + document.getElementById('zero').value;
+            
+    //     });
+    
+    // 	$('#one').click(function() {
+    // 	 document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('one').value;
+    // 	   // $('#results').html(1);
+    // 	});
+    
+    //     $('#two').click(function() {
+    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('two').value;
+    // 	});
+    	
+    // 	$('#three').click(function() {
+    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('three').value;
+    // 	});
+    	
+    // 	$('#four').click(function() {
+    // 	  document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('four').value;
+    // 	});
+    
+    //     $('#five').click(function() {
+    // 	  document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('five').value;
+    // 	});
+    	
+    // 	$('#six').click(function() {
+    // 	 document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('six').value;
+    // 	});
+    	
+    // 	$('#seven').click(function() {
+    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('seven').value;
+    // 	});
+    
+    //     $('#eight').click(function() {
+    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('eight').value;
+    // 	});
+    	
+    // 	$('#nine').click(function() {
+    // 	   document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('nine').value;
+    // 	});
+    	
