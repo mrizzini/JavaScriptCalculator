d15545d26755ee73bf4586e8c852efdee4436462
Matthew Rizzini <matthewrizzini@gmail.com>
2017-12-18 23:53:12 +0000

    clear functions working

--- index.js
+++ index.js
@@ -5,25 +5,50 @@ $(document).ready(function() {
     var numberInput = "";
     var numberInputTwo = "";
     var operatorInput = "";
-    var joinNumbers = [];
-    var joinNumbersTwo = [];
     var result = "";
-    var operation = "";
-
     	
 $( "#numbers > button").not("#clearCurrent, #clearAll").click(function() {
     numberInput +=  $(this).val();  
     $("#total").html(numberInput);
+     $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
+    // console.log(numberInput);
+    console.log(numberInputTwo + operatorInput + numberInput);
+});
+
+
+$("#clearCurrent").click(function(){
+    console.log("current input cleared");
+    numberInput = "";
+    $("#total").html(0);
+    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
     console.log(numberInput);
+    console.log(numberInputTwo);
+    console.log(operatorInput);
+});
+
+$("#clearAll").click(function(){
+    console.log("calculator cleared");
+    numberInput = "";
+    numberInputTwo = "";
+    operatorInput = "";
+    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
+    console.log(numberInput);
+    console.log(numberInputTwo);
+    console.log(operatorInput);
+
+    $("#total").html(0);
 });
 
 $("#operators > button").not("#equals").click(function(){
     operatorInput = $(this).val();
     numberInputTwo = numberInput;
     numberInput = "";
+    
     $("#total").html(operatorInput);
-    console.log(operatorInput);
-    console.log(numberInput);
+     $("#operationBox").html(numberInputTwo + operatorInput);
+    console.log(numberInput + operatorInput);
+    // console.log(operatorInput);
+    // console.log(numberInput);
     $("#total").html(operatorInput);
 
 });
@@ -45,70 +70,9 @@ $("#equals").click(function(){
     }
 });
 
-
-    	
-    // 	console.log(numberInput);
- 
-    	
-    // 	console.log(numbers);
     	
 });
 //above line ends the document ready
 
 
-
-    
-    //     $('#clearCurrent').click(function() {
-    // 	    $('#results').html(0);
-    // 	});
-    
-    //     $('#decimal').click(function() {
-    	    
-    // 	});
-    	
-    // 	$('#add').click(function() {
-    // 	    $('#results').html();
-    // 	});
-    
-    //     $('#zero').click(function() {
-    // 	     document.getElementById('results').innerHTML =  document.getElementById('results').innerHTML + document.getElementById('zero').value;
-            
-    //     });
-    
-    // 	$('#one').click(function() {
-    // 	 document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('one').value;
-    // 	   // $('#results').html(1);
-    // 	});
-    
-    //     $('#two').click(function() {
-    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('two').value;
-    // 	});
-    	
-    // 	$('#three').click(function() {
-    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('three').value;
-    // 	});
-    	
-    // 	$('#four').click(function() {
-    // 	  document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('four').value;
-    // 	});
-    
-    //     $('#five').click(function() {
-    // 	  document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('five').value;
-    // 	});
-    	
-    // 	$('#six').click(function() {
-    // 	 document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('six').value;
-    // 	});
-    	
-    // 	$('#seven').click(function() {
-    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('seven').value;
-    // 	});
-    
-    //     $('#eight').click(function() {
-    // 	    document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('eight').value;
-    // 	});
-    	
-    // 	$('#nine').click(function() {
-    // 	   document.getElementById('results').innerHTML = document.getElementById('results').innerHTML + document.getElementById('nine').value;
-    // 	});
     	
