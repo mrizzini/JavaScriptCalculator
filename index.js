09a58973c8eccd33d4b61edbe3ad2b1a3bb5c1fc
Matthew Rizzini <matthewrizzini@gmail.com>
2017-12-21 20:43:53 +0000

    added parseFloat for decimals and got clear all button working

--- index.js
+++ index.js
@@ -1,81 +1,69 @@
 /*global $*/
 
-$(document).ready(function() { 
-    
-    var numberInput = "";
-    var numberInputTwo = "";
-    var operatorInput = "";
-    var result = 0;
-    	
-$( "#numbers > button").not("#clearCurrent, #clearAll").click(function() {
-    
-    numberInput +=  $(this).val();  
-    
-    for (var i = 0; i < numberInput.length; i++ ) {
-        
-    }
-    
-    $("#total").html(numberInput);
-    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
-    // console.log(numberInput);
-    console.log(numberInputTwo + operatorInput + numberInput);
-});
-
-$("#operators > button").not("#equals").click(function(){
-    operatorInput = $(this).val();
-    numberInputTwo = numberInput;
-    numberInput = "";
-    
-    $("#total").html(operatorInput);
-    $("#operationBox").html(numberInputTwo + operatorInput);
-    console.log(numberInput + operatorInput);
-    // console.log(operatorInput);
-    // console.log(numberInput);
-    $("#total").html(operatorInput);
-
-});
-
-$("#equals").click(function(){
-    console.log("equals pushed");
-    if (operatorInput === "+") {
-        result = $("#total").html(parseInt(numberInputTwo) + parseInt(numberInput));
-        console.log(result);
-    } else if (operatorInput === "-") {
-        result = $("#total").html(parseInt(numberInputTwo) - parseInt(numberInput));
-        console.log(result);
-    } else if (operatorInput === "/") {
-        result = $("#total").html(parseInt(numberInputTwo) / parseInt(numberInput));
-        console.log(result);
-    } else if (operatorInput === "*") {
-        result = $("#total").html(parseInt(numberInputTwo) * parseInt(numberInput));
-        console.log(result);
-    }
-});
-
-$("#clearCurrent").click(function(){
-    console.log("current input cleared");
-    numberInput = "";
-    $("#total").html(0);
-    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
-    console.log(numberInput);
-    console.log(numberInputTwo);
-    console.log(operatorInput);
-});
-
-$("#clearAll").click(function(){
-    console.log("calculator cleared");
-    numberInput = "";
-    numberInputTwo = "";
-    operatorInput = "";
-    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
-    console.log(numberInput);
-    console.log(numberInputTwo);
-    console.log(operatorInput);
-
-    $("#total").html(0);
-});
-
-    	
-});
-//above line ends the document ready
-
+$(document).ready(function() {
+	var numberInput = "";
+	var result = 0;
+	var runningEquation = "";
+	
+	$("button").not("#clearCurrent, #clearAll, #equals").click(function() {
+		// console.log("button");
+		numberInput += ($(this).val());
+		console.log(numberInput);
+		$("#operationBox").html(numberInput);
+		var reg = new RegExp(/([\+\-\*\/])/, 'g');
+		runningEquation = numberInput.split(reg);
+		for (var i = 0; i < runningEquation.length; i++) {
+			$("#total").html(runningEquation[i]);
+		}
+	});
+	
+	$("#equals").click(function() {
+		// operators = numberInput.split((/|"+"|"-"/g));
+		// var reg = new RegExp(/(\+)/, 'g'); this works for one operator!!!
+		// var reg = new RegExp(/([\+\-\*\/])/, 'g'); tested putting these lines in button click
+		// runningEquation = numberInput.split(reg); tested putting these lines in button click
+		console.log(runningEquation);
+		// result = runningEquation[0];
+		console.log("so far, result is " + result);
+		console.log(runningEquation.length);
+		for (var i = 0; i < runningEquation.length; i++) {
+			// console.log(runningEquation[i]);
+			if (runningEquation[i] == "+") {
+				result += parseFloat(runningEquation[i + 1]);
+				i += 1;
+				console.log("after addition, result is " + result);
+			} else if (runningEquation[i] == "-") {
+				result -= parseFloat(runningEquation[i + 1]);
+				i += 1;
+				console.log("after subtraction, result is " + result);
+			} else if (runningEquation[i] == "*") {
+				result *= parseFloat(runningEquation[i + 1]);
+				i += 1;
+				console.log("after multiplication, result is " + result);
+			} else if (runningEquation[i] == "/") {
+				result /= parseFloat(runningEquation[i + 1]);
+				i += 1;
+				console.log("after division, result is " + result);
+			} else {
+				result += parseFloat(runningEquation[i]);
+			}
+		}
+		console.log("final result is " + result);
+		$("#total").html(result);
+	});
+	
+	//  $("#clearCurrent").click(function() {
+	//     console.log("current input cleared");
+	//     console.log("after clear current, " + runningEquation);
+	//  });
+	
+	$("#clearAll").click(function() {
+		numberInput = "";
+		runningEquation = "";
+		result = "";
+		$("#operationBox").html(0);
+		$("#total").html(0);
+		console.log("ENITRE CALCULATOR RESET, " + runningEquation);
+	});
+	
+}); // this line ends document ready
--- test.js
+++ test.js
@@ -3,130 +3,78 @@
 $(document).ready(function() { 
     
     var numberInput = "";
+    var numberInputTwo = "";
+    var operatorInput = "";
     var result = 0;
-    var runningEquation = "";
+    	
+$( "#numbers > button").not("#clearCurrent, #clearAll").click(function() {
     
+    numberInput +=  $(this).val();  
     
-    $("button").not("#clearCurrent, #clearAll, #equals").click(function() {
-    // console.log("button");
-    numberInput += ($(this).val());
-    console.log(numberInput);
-    $("#total").html(numberInput);
-
-});
-    
-     $("#equals").click(function() {
-    // operators = numberInput.split((/|"+"|"-"/g));
-    // var reg = new RegExp(/(\+)/, 'g'); this works for one operator!!!
-    var reg = new RegExp(/([\+\-\*\/])/, 'g');
-    runningEquation = numberInput.split(reg);
-    console.log(runningEquation);
-    // result = runningEquation[0];
-    console.log("so far, result is " + result);
-    console.log(runningEquation.length);
-    
-    for (var i = 0; i < runningEquation.length; i++) {
-        // console.log(runningEquation[i]);
+    for (var i = 0; i < numberInput.length; i++ ) {
         
-        if (runningEquation[i] == "+") {
-            result += parseInt(runningEquation[i + 1]);
-            i += 1;
-            console.log("after addition, result is " + result);
-        } else if (runningEquation[i] == "-") {
-            result -= parseInt(runningEquation[i + 1]);
-            i += 1;
-            console.log("after subtraction, result is " + result);
-        } else if (runningEquation[i] == "*") {
-            result *= parseInt(runningEquation[i + 1]);
-            i += 1;
-        } else if (runningEquation[i] == "/") {
-            result /= parseInt(runningEquation[i + 1]);
-            i += 1;
-        }
-        else {
-            result += parseInt(runningEquation[i]);
-        }
     }
-    console.log("final result is " + result);
-    $("#total").html(result);
-     });
-     
-    //  $("#clearCurrent").click(function() {
-    //     console.log("current input cleared");
-    //         for (var i = 0; i < runningEquation.length; i++) {
-    //             runningEquation = runningEquation[i-1];
-    //         }
-    //         $("#total").html(runningEquation);
-    //         console.log(runningEquation);
-    //  });
-    	
-// $( "#numbers > button").not("#clearCurrent, #clearAll").click(function() {
     
-//     numberInput +=  $(this).val();  
+    $("#total").html(numberInput);
+    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
+    // console.log(numberInput);
+    console.log(numberInputTwo + operatorInput + numberInput);
+});
+
+$("#operators > button").not("#equals").click(function(){
+    operatorInput = $(this).val();
+    numberInputTwo = numberInput;
+    numberInput = "";
     
-//     for (var i = 0; i < numberInput.length; i++ ) {
-        
-//     }
-    
-//     $("#total").html(numberInput);
-//     $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
-//     // console.log(numberInput);
-//     console.log(numberInputTwo + operatorInput + numberInput);
-// });
+    $("#total").html(operatorInput);
+    $("#operationBox").html(numberInputTwo + operatorInput);
+    console.log(numberInput + operatorInput);
+    // console.log(operatorInput);
+    // console.log(numberInput);
+    $("#total").html(operatorInput);
 
-// $("#operators > button").not("#equals").click(function(){
-//     operatorInput = $(this).val();
-//     numberInputTwo = numberInput;
-//     numberInput = "";
-    
-//     $("#total").html(operatorInput);
-//     $("#operationBox").html(numberInputTwo + operatorInput);
-//     console.log(numberInput + operatorInput);
-//     // console.log(operatorInput);
-//     // console.log(numberInput);
-//     $("#total").html(operatorInput);
+});
 
-// });
+$("#equals").click(function(){
+    console.log("equals pushed");
+    if (operatorInput === "+") {
+        result = $("#total").html(parseFloat(numberInputTwo) + parseFloat(numberInput));
+        console.log(result);
+    } else if (operatorInput === "-") {
+        result = $("#total").html(parseInt(numberInputTwo) - parseInt(numberInput));
+        console.log(result);
+    } else if (operatorInput === "/") {
+        result = $("#total").html(parseInt(numberInputTwo) / parseInt(numberInput));
+        console.log(result);
+    } else if (operatorInput === "*") {
+        result = $("#total").html(parseInt(numberInputTwo) * parseInt(numberInput));
+        console.log(result);
+    }
+});
 
-// $("#equals").click(function(){
-//     console.log("equals pushed");
-//     if (operatorInput === "+") {
-//         result = $("#total").html(parseInt(numberInputTwo) + parseInt(numberInput));
-//         console.log(result);
-//     } else if (operatorInput === "-") {
-//         result = $("#total").html(parseInt(numberInputTwo) - parseInt(numberInput));
-//         console.log(result);
-//     } else if (operatorInput === "/") {
-//         result = $("#total").html(parseInt(numberInputTwo) / parseInt(numberInput));
-//         console.log(result);
-//     } else if (operatorInput === "*") {
-//         result = $("#total").html(parseInt(numberInputTwo) * parseInt(numberInput));
-//         console.log(result);
-//     }
-// });
+$("#clearCurrent").click(function(){
+    console.log("current input cleared");
+    numberInput = "";
+    $("#total").html(0);
+    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
+    console.log(numberInput);
+    console.log(numberInputTwo);
+    console.log(operatorInput);
+});
 
-// $("#clearCurrent").click(function(){
-//     console.log("current input cleared");
-//     numberInput = "";
-//     $("#total").html(0);
-//     $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
-//     console.log(numberInput);
-//     console.log(numberInputTwo);
-//     console.log(operatorInput);
-// });
+$("#clearAll").click(function(){
+    console.log("calculator cleared");
+    numberInput = "";
+    numberInputTwo = "";
+    operatorInput = "";
+    $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
+    console.log(numberInput);
+    console.log(numberInputTwo);
+    console.log(operatorInput);
 
-// $("#clearAll").click(function(){
-//     console.log("calculator cleared");
-//     numberInput = "";
-//     numberInputTwo = "";
-//     operatorInput = "";
-//     $("#operationBox").html(numberInputTwo + operatorInput + numberInput);
-//     console.log(numberInput);
-//     console.log(numberInputTwo);
-//     console.log(operatorInput);
-
-//     $("#total").html(0);
-// });
+    $("#total").html(0);
+});
 
     	
 });
+//above line ends the document ready
